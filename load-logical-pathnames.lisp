0e0cb53efa238ed0976f8359b82f4e5f50b80984
shapiro <shapiro>
2013-08-20 15:33:46 +0000

    Added license and copyright information.

--- load-logical-pathnames.lisp
+++ load-logical-pathnames.lisp
@@ -1,3 +1,43 @@
+;;; -*- Mode: Lisp; Syntax: Common-Lisp; Package: SNEPS; Base: 10 -*-
+
+;; Copyright (C) 1984--2013
+;; Research Foundation of State University of New York
+
+;; Version: $Id: load-logical-pathnames.lisp,v 
+
+;; This file is part of SNePS.
+
+;; $BEGIN LICENSE$
+
+;; 
+;; The contents of this file are subject to the University at
+;; Buffalo Public License Version 1.0 (the "License"); you may
+;; not use this file except in compliance with the License. You
+;; may obtain a copy of the License at http://www.cse.buffalo.
+;; edu/sneps/Downloads/ubpl.pdf.
+;; 
+;; Software distributed under the License is distributed on an
+;; "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express
+;; or implied. See the License for the specific language gov
+;; erning rights and limitations under the License.
+;; 
+;; The Original Code is SNePS 2.7.
+;; 
+;; The Initial Developer of the Original Code is Research Foun
+;; dation of State University of New York, on behalf of Univer
+;; sity at Buffalo.
+;; 
+;; Portions created by the Initial Developer are Copyright (C)
+;; 2013 Research Foundation of State University of New York, on
+;; behalf of University at Buffalo. All Rights Reserved.
+;; 
+;;  
+;; 
+;; 
+
+
+;; $END LICENSE$
+
 ;; This file actually installs translations in the logical pathname
 ;; package.  This is a separate file so that the acl standalone can
 ;; also reestablish the translations (which, starting with version 4.3
